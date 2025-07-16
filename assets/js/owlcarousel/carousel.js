c7b707ae6a44ce53c3ae7446955dfa9a32552fe6
ifatinha <ifatinha99@gmail.com>
2025-07-16 14:51:19 -0300

    feat(carousel): HTML e SASS finalizados, aguardando inserção do conteúdo

--- assets/js/owlcarousel/carousel.js
+++ assets/js/owlcarousel/carousel.js
@@ -1,9 +1,33 @@
 export function carouselTestimonial() {
   $(document).ready(function () {
     $("#testimonialCarousel").owlCarousel({
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      center: true,
       dots: false,
-      margin: 10,
+      items: 2,
+      margin: 40,
+      nav: false,
       loop: true,
+      smartSpeed: 800,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 1,
+        },
+        992: {
+          items: 1,
+        },
+        1200: {
+          items: 2,
+        },
+        1400: {
+          items: 2,
+        },
+      },
     });
   });
 }
